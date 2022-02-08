a41fc18652d222409df54a5a7b2adc2b66316d97
bibek10550 <84659279+bibek10550@users.noreply.github.com>
2022-02-08 15:52:33 +0545

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,46 @@
+function validate(){
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if(username=="bibek"&& password=="sah"){
+        // window.open("https://www.instagram.com/bibeksha1432/");
+        // return false;
+        // alert("login succesfully");
+        window.open("/cover/cover.html");
+        // return false;
+    }else{
+        alert("login failed");
+        // return false;
+    }
+}
+
+const passInput = document.querySelector('[data-pwd-input]')
+const toggleBtn = document.querySelector('[data-pwd-toggle]')
+
+let showPassword = false
+
+passInput.addEventListener('input', showToggleButton)
+passInput.addEventListener('focus', () => {
+    if (showPassword && passInput.value.length < 1) togglePasswordVisible()
+})
+
+toggleBtn.addEventListener('click', () => {
+    togglePasswordVisible()
+    passInput.focus()
+})
+
+function showToggleButton() {
+    if (passInput.value.length > 0) toggleBtn.style.display = 'block'
+    else toggleBtn.style.display = ''
+}
+
+function togglePasswordVisible() {
+    showPassword = !showPassword
+    if (showPassword) {
+        toggleBtn.classList = 'fas fa-eye-slash'
+        passInput.setAttribute('type', 'text')
+    } else {
+        toggleBtn.classList = 'fas fa-eye'
+        passInput.setAttribute('type', 'password')
+    }
+}
+
